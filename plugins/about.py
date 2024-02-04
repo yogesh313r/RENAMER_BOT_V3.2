c8968f63936e5db82f89f4561ffb831949cf19c1
yogesh313r <149294443+yogesh313r@users.noreply.github.com>
2024-02-04 13:57:13 +0530

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -1,6 +1,6 @@
 import os 
 from pyrogram import Client, filters
-token = os.environ.get('TOKEN','')
+token = os.environ.get('TOKEN','6475149749:AAEQt9M91pjiNCBF_lkxgVEXdq77S1lT_n0')
 botid = token.split(':')[0]
 from helper.database import botdata, find_one, total_user
 from helper.progress import humanbytes
